47a2728965cfbc991e26d85a2efb81970746ac00
HyundeokPark <hpotter1993@naver.com>
2023-01-28 20:06:12 +0900

    wip : first study

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,42 @@
 package study;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+    @Test
+    void split() {
+        String target = "1,2";
+        String[] splitedTarget = target.split(",");
+        assertThat(splitedTarget).contains("1","2");
+
+        target = "1";
+        splitedTarget = target.split(",");
+        assertThat(splitedTarget).containsExactly("1");
+    }
+
+    @Test
+    void substring(){
+        String afterFunc = "(1,2)".substring(1,"(1,2)".length()-1);
+        assertThat(afterFunc).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 인덱스가 없을때 에러발생")
+    void charAt() {
+        assertThatThrownBy( () ->{
+            int index = 10;
+            char result = "abc".charAt(index);
+        }).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("String index out of range: 10");
+
     }
 }
