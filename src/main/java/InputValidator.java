384b239d29ada55e069b3e333dcfb648636f735b
박현덕 <hpotter1993@wisebirds.co.kr>
2024-07-10 08:04:26 +0900

    사용 자 입력 3자리수 검증

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -1,7 +1,13 @@
 import java.util.List;
 
 public class InputValidator {
+    private static final int VALID_SIZE = 3;
+
     public static boolean validateDuplication(List<Integer> input) {
         return input.stream().distinct().count() == input.size();
+    }
+
+    public static boolean validateInputSize(List<Integer> input) {
+        return input.size() ==VALID_SIZE;
     }
 }
--- src/test/java/InputValidatorTest.java
+++ src/test/java/InputValidatorTest.java
@@ -14,6 +14,20 @@ public class InputValidatorTest {
         assertThat(InputValidator.validateDuplication(input)).isEqualTo(result);
     }
 
+    @ParameterizedTest(name = "사용자의 입력값에 중복을 확인한다.")
+    @MethodSource("provideVariousSizeUserInput")
+    void validateInputSize(List<Integer> input, boolean result) {
+        assertThat(InputValidator.validateInputSize(input)).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> provideVariousSizeUserInput() {
+        return Stream.of(
+                Arguments.of(List.of(1,2,3), true),
+                Arguments.of(List.of(1,2), false),
+                Arguments.of(List.of(1,2,4,5), false)
+        );
+    }
+
     private static Stream<Arguments> provideUserInput() {
         return Stream.of(
                 Arguments.of(List.of(1,2,3), true),
